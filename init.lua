9422c4c377d2580a8b4976d58d8b9aca5a8fb43c
DashDash Studios <79600449+dashdash-studios@users.noreply.github.com>
2023-03-31 14:48:23 -0700

    Update init.lua
    Added 
    - v-snip,
    - lsp-signature
    
    Replaced catppuccin with one dark's deep theme
    
    Configured nvim-cokeline

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 ---         NvimJustBetter           ---
+---                                  ---
 ---      THE best neovim config      ---
 ---   No downloads, just copy+paste  ---
 ---                                  ---
@@ -29,6 +30,10 @@ vim.cmd([[
 	vmap <C-x> "+c
 	vmap <C-v> c<ESC>"+p
 	imap <C-v> <C-r><C-o>+
+	set tabstop=4
+	set shiftwidth=4
+	set expandtab
+    autocmd CursorHold,CursorHoldI * update
 ]])
 
 -- Install Plugins --
@@ -43,8 +48,14 @@ require("lazy").setup({
 	'hrsh7th/cmp-nvim-lsp', -- Bridges LspConfig and Cmp
 
 	'hrsh7th/nvim-cmp', -- Auto-completion
+    
+    'hrsh7th/cmp-vsnip', -- Bridges Vsnip and Cmp
+    
+    'hrsh7th/vim-vsnip', -- Snippets
+
+    'ray-x/lsp_signature.nvim', -- Function Arguments
  
-	{ "catppuccin/nvim", name = "catppuccin" }, -- Neovim Theme
+	'navarasu/onedark.nvim', -- Neovim Theme
 
 	{ 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate"}, -- Better syntax highlighting
  
@@ -82,6 +93,8 @@ require("lazy").setup({
 	--             Add your own plugins!              --
 	--   Use provided lazy.nvim install from github   --
 	--  Or just go below and put 'author/repository', --
+    
+    	-- 'manzeloth/live-server',
  
 })
 
@@ -92,9 +105,20 @@ require("lazy").setup({
 require("mason").setup()
 require("mason-lspconfig").setup()
 
+-- onedark.nvim (Neovim Theme)
+require('onedark').setup {
+    style = 'deep'
+}
+
 -- nvim-cmp (Autocomplete)
 local cmp = require("cmp")
 cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      end,
+    },
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -104,28 +128,83 @@ cmp.setup({
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
+        { name = 'vsnip' },
 	}, {
 		{ name = 'buffer' }
 	}),
 })
 
--- catppuccin (Theme)
-require("catppuccin").setup({
-	flavour = "macchiato",
-	transparent_background = true,
-	coc_nvim = true,
-})
-
 -- lualine (Statusline)
-require('lualine').setup({
-	theme = "catppuccin"
-})
+require('lualine').setup()
 
 -- neo-tree (File Explorer)
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 -- cokeline (Tabline)
-require('cokeline').setup()
+local get_hex = require('cokeline/utils').get_hex
+
+local yellow = vim.g.terminal_color_3
+
+require('cokeline').setup {
+    default_hl = {
+        fg = function(buffer)
+          return
+            buffer.is_focused
+            and get_hex('Normal', 'fg')
+             or get_hex('Comment', 'fg')
+        end,
+        bg = get_hex('ColorColumn', 'bg'),
+      },
+
+      components = {
+        {
+          text = ' ',
+          bg = get_hex('Normal', 'bg'),
+        },
+        {
+          text = '',
+          fg = get_hex('ColorColumn', 'bg'),
+          bg = get_hex('Normal', 'bg'),
+        },
+        {
+          text = function(buffer)
+            return buffer.devicon.icon
+          end,
+          fg = function(buffer)
+            return buffer.devicon.color
+          end,
+        },
+        {
+          text = ' ',
+        },
+        {
+          text = function(buffer) return buffer.filename .. '  ' end,
+          style = function(buffer)
+            return buffer.is_focused and 'bold' or nil
+          end,
+        },
+        {
+          text = '',
+          delete_buffer_on_left_click = true,
+        },
+        {
+          text = '',
+          fg = get_hex('ColorColumn', 'bg'),
+          bg = get_hex('Normal', 'bg'),
+        },
+    },
+    sidebar = {
+        filetype = 'neo-tree',
+        components = {
+            {
+                text = "    Neo-tree",
+                fg = vim.g.terminal_color_3,
+                bg = get_hex("NeoTreeNormal", 'bg'),
+                style = 'bold'
+            }
+        }   
+    }
+}
 
 -- auto-pairs (Closes parentheses, quotes, etc.)
 require("nvim-autopairs").setup {}
@@ -135,13 +214,32 @@ cmp.event:on(
   cmp_autopairs.on_confirm_done()
 )
 
+-- LSP Signature (Function Arguments)
+require("lsp_signature").setup()
+
 -- Set Colorscheme --
-vim.cmd.colorscheme "catppuccin"
+require('onedark').load()
 vim.cmd([[ set number ]]) -- Numbered Lines
 
 
---         SETUP LSP SERVERS HERE         --
---    Include Cmp Capabilities in setup   --
-
+-- LSP Setup Helpers --
 local lsp = require("lspconfig")
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lsp_setup = {
+  on_attach = function(client, bufnr)
+    require("lsp_signature").on_attach({
+      bind = true, -- This is mandatory, otherwise border config won't get registered.
+      handler_opts = {
+        border = "rounded"
+      }
+    }, bufnr)
+  end,
+  capabilities=capabilities,
+}
+function add(t1, t2)
+    return table.move(t2, 1, #t2, #t1 + 1, t1)
+end
+
+--               SETUP LSP SERVERS HERE                   --
+--      Example Setup: lsp.pyright.setup(lsp_setup)       --
+-- With Options: lsp.server.setup(add(lsp_setup, {opts})) --
